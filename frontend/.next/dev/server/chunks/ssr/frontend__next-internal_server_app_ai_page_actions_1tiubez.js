4a49909a3fb254d7e28fc18cbc5a4a49631b2026
David <88470336+MrKingDev@users.noreply.github.com>
2026-09-21 06:05:29 -0400

    Untrack node_modules and .next

--- frontend/.next/dev/server/chunks/ssr/frontend__next-internal_server_app_ai_page_actions_1tiubez.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/frontend/.next-internal/server/app/ai/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=frontend__next-internal_server_app_ai_page_actions_1tiubez.js.map
--- frontend/.next/dev/static/chunks/frontend_1anvha4._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_1mojsay._.js","static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_1jwu2nk._.js","static/chunks/1f6v_next_dist_compiled_next-devtools_index_0q1szk_.js","static/chunks/1f6v_next_dist_compiled_react-dom_1ofrxr9._.js","static/chunks/1f6v_next_dist_compiled_react-server-dom-turbopack_1xpedue._.js","static/chunks/1f6v_next_dist_compiled_1q1-fao._.js","static/chunks/1f6v_next_dist_client_1km-yf9._.js","static/chunks/1f6v_next_dist_0xnte6g._.js","static/chunks/0zix_@swc_helpers_cjs_0hi-e47._.js"],
-    source: "entry"
-});
